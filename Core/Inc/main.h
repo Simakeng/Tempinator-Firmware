2d9f7f071292d95033ec44324d4bc918a0382d55
Simakeng <simakeng@outlook.com>
2024-05-29 16:34:46 +0800

    Init repo with STM32CubeMX generated code.

---
+++ Core/Inc/main.h
@@ -0,0 +1,89 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BOOT_MODE_Pin GPIO_PIN_14
+#define BOOT_MODE_GPIO_Port GPIOC
+#define DISP_NRST_Pin GPIO_PIN_15
+#define DISP_NRST_GPIO_Port GPIOC
+#define IO_LATCH_Pin GPIO_PIN_2
+#define IO_LATCH_GPIO_Port GPIOA
+#define IO_NCLR_Pin GPIO_PIN_3
+#define IO_NCLR_GPIO_Port GPIOA
+#define IO_NOE_Pin GPIO_PIN_4
+#define IO_NOE_GPIO_Port GPIOA
+#define S_CLR_Pin GPIO_PIN_6
+#define S_CLR_GPIO_Port GPIOA
+#define S_NEXT_Pin GPIO_PIN_8
+#define S_NEXT_GPIO_Port GPIOA
+#define DISP_CS_Pin GPIO_PIN_11
+#define DISP_CS_GPIO_Port GPIOA
+#define DISP_DC_Pin GPIO_PIN_12
+#define DISP_DC_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
